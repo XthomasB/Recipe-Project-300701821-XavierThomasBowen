2827773f228a2f2f424d5efc223ad89e7053a69f
Xavier <jt.xbowen@gmail.com>
2016-11-17 18:18:55 -0500

    First Commit

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,13 +1,13 @@
-﻿CREATE TABLE [dbo].[Adding] (
-    [RecipeID]     INT           NOT NULL,
-    [Name]         NVARCHAR (50) NOT NULL,
-    [SubmittiedBy] NVARCHAR (50) NOT NULL,
-    [Category]     NVARCHAR (50) NOT NULL,
-    [PrepareC]     NVARCHAR (50) NULL,
-    [RecipeDes]    NVARCHAR (50) NULL,
-    [Ingredient]   NVARCHAR (50) NULL,
-    [Qauntity]     NVARCHAR (50) NULL,
-    [Units]        NVARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([RecipeID] ASC)
-);
+﻿
+select * from Adding
+delete from Adding where RecipeID = 3; 
 
+
+insert into Adding values (1,'Waffle Crunch','Haki D','Breakfast','10 minutes','fghg','gfh','2','N/A')
+insert into Adding values (2,'Warm tongue','Snickers B','Lunch','30 minutes','fghg','gfh','1','N/A')
+insert into Adding values (3,'Alfredo Swirls','Meghan C','Dinner','17 Minutes','fghg','gfh','2','N/A')
+insert into Adding values (4,'Steak Burger','Xavier T','Dessert','gfhfg','fghg','gfh','1','N/A')
+insert into Adding values (5,'Ultimate Nachos','Meghan C','Dessert','gfhfg','fghg','gfh','1','N/A')
+insert into Adding values (6,'Pizza Delux','Mario L','Dinner','gfhfg','fghg','gfh','4','N/A')
+insert into Adding values (7,'Farmers Bagel','Tim H','Breakfast','8 minutes','A grilled breakfast sandwhich bagel','Egg, Bagel, Lettuce','1','N/A')
+insert into Adding values (8,'Banana Split','Meghan C','Dessert','5 Minutes','Banana and Ice Cream Boat!','Icecream, Banana, syrups, pinapples, strawberry','2','N/A')
