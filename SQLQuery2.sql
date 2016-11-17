a9a97a113387e1676cbb3313b2bf78cb4b7d7dfc
Xavier <jt.xbowen@gmail.com>
2016-11-17 12:43:07 -0500

    Add project files.

---
+++ SQLQuery2.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Adding] (
+    [RecipeID]     INT           NOT NULL,
+    [Name]         NVARCHAR (50) NOT NULL,
+    [SubmittiedBy] NVARCHAR (50) NOT NULL,
+    [Category]     NVARCHAR (50) NOT NULL,
+    [PrepareC]     NVARCHAR (50) NULL,
+    [RecipeDes]    NVARCHAR (50) NULL,
+    [Ingredient]   NVARCHAR (50) NULL,
+    [Qauntity]     NVARCHAR (50) NULL,
+    [Units]        NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([RecipeID] ASC)
+);
+
